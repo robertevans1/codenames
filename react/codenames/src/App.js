27488a4b88781f62e9dc851609a0fff4c83c3ba1
Rob Evans <robertsevans10@gmail.com>
2024-08-21 19:33:56 +0100

    fetch words from django

--- react/codenames/src/App.js
+++ react/codenames/src/App.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import './App.css'; // Assuming your styles are in App.css
+import { useState, useEffect } from 'react';
 
 function WordButton({ word }) {
   return <button>{word}</button>;
 }
 
 function App() {
-  const buttonLabels = [
-    'Button 1',
-    'Button 2',
-    'Button 3',
-    'Button 4',
-    'Button 5',
-    'Button 666',
-    'Button 7',
-    'Button 8',
-    'Button 9',
-  ];
+
+  const [buttonLabels, setButtonLabels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+     // Fetch button labels from the API
+     const fetchButtonLabels = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/games/1/words/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const labels = data.map((item) => item.word);
+        setButtonLabels(labels);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchButtonLabels();
+  }, []); 
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="App">
