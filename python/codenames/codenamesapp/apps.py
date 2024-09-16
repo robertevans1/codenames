1cf9a271e5317c4ff043683e5107c0524270efd7
Rob Evans <robertsevans10@gmail.com>
2024-09-16 11:58:05 +0100

    change naming

--- python/codenames/codenamesapp/apps.py
+++ python/codenames/codenamesapp/apps.py
@@ -26,12 +26,13 @@ class CodenamesappConfig(AppConfig):
         else:
             print(f'{file_path} already exists.')
 
+        print_file_paths('/data')
         word_embedding_data.load_data("/data/glove.6B.300d.txt")
 
 # Paths
 file_path = '/data/glove.6B.300d.txt'
 zip_url = 'https://zenodo.org/records/4925376/files/glove.6B.300d.zip'
-zip_path = '/data/glove.6B.zip'
+zip_path = '/data/glove.6B.300d.zip'
 
 def download_file(url, dest_path):
     response = requests.get(url)
@@ -42,5 +43,12 @@ def download_file(url, dest_path):
 def extract_zip(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
+
+
+def print_file_paths(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            print(os.path.join(root, file))
+
 
 
